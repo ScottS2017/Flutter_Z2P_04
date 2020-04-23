9a5fcc2d61eba11eaafa8b2c8e3f096ae4bfabc8
Scott Stoll <scottstoll2017@gmail.com>
2020-04-23 10:17:21 -0400

    Added doc comments to the routes examples

--- lib/widgets/examples/navigation_and_routes_example.dart
+++ lib/widgets/examples/navigation_and_routes_example.dart
@@ -47,7 +47,70 @@ class _NavigationAndRoutesState extends State<NavigationAndRoutes> {
               /// Material Page Route Button ***********************
               RaisedButton(
                 onPressed: () {
-                  // ignore: strict_raw_type
+
+                  /// There is more than one syntax that will work with a
+                  /// MaterialPageRoute, but the one we want to use is the
+                  /// one that is going to specifically check the current
+                  /// context.
+                  ///
+                  /// *** Mnemonic Devices and Concepts ***
+                  ///
+                  /// - Use a Navigator, since we're Navigating
+                  ///
+                  /// - What are we Navigating? The current situation, or
+                  ///       context. So it's a Navigator OF our context.
+                  ///
+                  /// - When we Navigate we keep a stack of past pages.
+                  ///      New pages go on top of the stack so that when
+                  ///      we go back, we take the most recent one off
+                  ///      of the stack. This is how a back button works.
+                  ///
+                  /// - Are we going somewhere new (pushing a new
+                  ///      Route onto the stack) or are we going back (popping
+                  ///      the most recent Route off the stack)?
+                  ///
+                  /// - Here, we're going somewhere new so we're going to
+                  ///       push.
+                  ///
+                  /// - HOT TIP: The push is not something that happens
+                  ///      instantly, it's what we call a Future (sometimes
+                  ///      called a promise elsewhere). This means it needs
+                  ///      to say what type of thing is going to be returned
+                  ///      in the Future. That's what the < > is for. However,
+                  ///      this time we aren't going to return anything, so
+                  ///      we just say "void" (we have to say something).
+                  ///
+                  /// - The most common way to route to a new page is by
+                  ///       using a MaterialPageRoute. It has to build the page,
+                  ///       but under what conditions is it going to build this
+                  ///       page? We'll find the current conditions, you guessed
+                  ///       it, in the context.
+                  ///
+                  /// - The builder is going to go build something and then
+                  ///       bring it back to us, so it returns something ( here
+                  ///       we're using a lambda => for this). What it will
+                  ///       return is **the constructor** for whatever page we
+                  ///       want to go to.
+                  ///
+                  /// - The constructor is a constant constructor, so use const
+                  ///       when calling it.
+                  ///
+                  /// So, in English:
+                  ///       _ We're going to use a Navigator to navigate
+                  ///       within the current context (Navigator OF).
+                  ///       - We're going to a new page, so we push it
+                  ///       onto the stack.
+                  ///       - We're not returning anything (void).
+                  ///       - To go anywhere in real life we need a route to
+                  ///       get there and this is no different, so we're using
+                  ///       a MaterialPageRoute, to route through the current
+                  ///       context.
+                  ///       - Before we can go to the new page we need to build
+                  ///       it.
+                  ///       - Under what conditions do we build? The context
+                  ///       - We need to tell the builder what page to bring
+                  ///       back to us, and call the constructor so the builder
+                  ///       can construct it.
                   Navigator.of(context).push<void>(
                     MaterialPageRoute(
                       builder: (context) => const HomeOrange(),
@@ -112,12 +175,47 @@ class _NavigationAndRoutesState extends State<NavigationAndRoutes> {
 
               /// Named Route Button ***********************
               RaisedButton(
+
+                /// Named routes are similar to MaterialPageRoutes
+                /// but the main difference is in where they get
+                /// the name of the page to build. There is a Map
+                /// (dictionary) of route names in the MaterialApp
+                /// Widget (here it's in the main.dart file).
+                ///
+                /// The pushNamed technique of routing will take
+                /// whatever string you give it here and go look
+                /// up which constructor it needs to call from the
+                /// routes Map in the MaterialAppWidget.
+                ///
+                /// Why do this? Imagine if you had an app with
+                /// 50 screens, each one is a route. Now, you need
+                /// to change something that will affect how 20 of
+                /// them work. You could dig through all of these files
+                /// to find every single time you created a route to
+                /// the affected area, or you could just change one
+                /// listing in a routes Map.
+                ///
+                /// HOT TIP: If your routes table is large, put it
+                /// somewhere else and call it into the routes Map
+                /// by name. IE instead of:
+                ///
+                ///           routes: {
+                ///           '/': (context) => const Welcome(),
+                ///           '/routes_one': (context) => const HomeTeal(),
+                ///           '/routes_two': (context) => const HomeOrange(),
+                ///           },
+                ///
+                /// Put your Map somewhere else and use:
+                ///
+                ///             routes: placeWhereMyMapIsStashed,
+                ///
+                ///
                 /// Danger! If you use popAndPushNamed here instead of
                 /// pushNamed, it will pop this page off the stack before
                 /// the next page is loaded. This will cause this page to
                 /// not be in the stack when you try to go back (pop)
-                /// from the next page and you'll end up at the
-                /// Welcome page instead of coming back here.
+                /// and you'll skip over this page, end up at the
+                /// page before this one instead of coming back here.
                 onPressed: () => Navigator.of(context).pushNamed(
                   '/routes_two',
                 ),
@@ -177,6 +275,9 @@ class _NavigationAndRoutesState extends State<NavigationAndRoutes> {
               /// Pop Button ***********************
               RaisedButton(
                 onPressed: () {
+                  ///
+                  /// This is the easy one. Navigate the context and
+                  /// pop the top page off the stack.
                   Navigator.of(context).pop();
                 },
                 color: AppColors.darkThemeWhiteText87pctOpacity,
